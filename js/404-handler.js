1765b3d5eb9fccef0d532735548336cfe02cb3e4
miqdamsyiamn <miqdamsyiamn@gmail.com>
2025-04-21 18:01:29 +0700

    test

--- js/404-handler.js
+++ js/404-handler.js
@@ -1,14 +1,12 @@
 // Check if the requested page exists
 function handle404() {
-    fetch(window.location.pathname)
-        .then(response => {
-            if (!response.ok && response.status === 404) {
-                window.location.href = '/404.html';
-            }
-        })
-        .catch(() => {
-            window.location.href = '/404.html';
-        });
+    // Get current URL
+    const currentPath = window.location.pathname;
+    
+    // If we're not on the 404 page and the path doesn't exist
+    if (currentPath !== '/404.html' && currentPath !== '/index.html' && currentPath !== '/') {
+        window.location.href = 'http://localhost:8000/404.html';
+    }
 }
 
 // Run on page load
